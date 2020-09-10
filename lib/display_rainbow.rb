1bc9a2e9275e812d6e2e05f2f765b86ba347675a
jocular-firmware-2768 <yalvarado924@gmail.com>
2020-09-10 17:53:28 +0000

    Automatically backed up by Learn

--- lib/display_rainbow.rb
+++ lib/display_rainbow.rb
@@ -1,3 +1,3 @@
 def display_rainbow(colors)
-  puts "R: #{[0]}, O: #{[1]}, Y: #{[2]}, G: #{[3]}, B: #{[4]}, I: #{[5]}, V: #{[6]}"
+  puts "R: #{red}, O: #{orange}, Y: #{yellow}, G: #{green}, B: #{blue}, I: #{indigo}, V: #{violet}"
 end
